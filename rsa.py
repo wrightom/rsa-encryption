dbf384f85f1a7602bd785ffdb5bb1eebb0361870
Tom Wright <tomnwright@outlook.com>
2019-11-25 23:51:13 +0000

    Should Be Working

--- rsa.py
+++ rsa.py
@@ -1,16 +1,31 @@
 #RSA Algorithm
 #By Tom Wright
 import tools
+import euclidean
 
 def encrypt (m, e, n):
-  return (m ** e) % n
+    return (m ** e) % n
 
 def decrypt (c, d, n):
-  return (c ** d) % n
+    return (c ** d) % n
 
 
 #algorithm instance
 
 #generate two large random prime numbers, p & q
 #(using max 10 dp numbers)
-p, q = tools.RandomPrime(999999999,9999999999), tools.RandomPrime(999999999,9999999999)
+u, l = 999999, 99999999
+p, q = tools.RandomPrime(u,l), tools.RandomPrime(u,l)
+n = p * q
+t = tools.primeTotient(p,q) # t = phi(n), Euler Totient Function
+e = tools.getE(t)
+print(p,q,n,t,e, "p,q,n,t,e")
+
+d = euclidean.mod_inv(e,t)
+
+m = 235
+c = encrypt(m,e,n)
+print('ENCRYPT {} -> {}'.format(m,c))
+k = decrypt(c,d,n)
+print('DECRYPT {} -> {}'.format(c,k))
+input()
--- tools.py
+++ tools.py
@@ -4,48 +4,63 @@ import random
 
 def isPrime(n):
   
-  if (n % 2) == 0:
-    return False
-  
-  target = int(math.sqrt(n)) + 1
-  for i in range(3, target):
-    if (n % i) == 0:
-      return False
+    if (n % 2) == 0:
+        return False
 
-  return True
+    target = int(math.sqrt(n)) + 1
+    for i in range(3, target):
+        if (n % i) == 0:
+            return False
+
+    return True
 
 def coPrime(a,b):
-  return math.gcd(a,b) == 1
+    return math.gcd(a,b) == 1
 
 def RandomPrime (lower, upper):
-  #efficient up to around 10 decimal places
-  if upper-lower <= 1:
-    return None
-    
-  while True:
-    c = random.randint(lower,upper) #candidate
-    
-    if (isPrime(c)):
-      return c
+    #efficient up to around 10 decimal places
+    if upper-lower <= 1:
+        return None
+
+    while True:
+        c = random.randint(lower,upper) #candidate
+
+        if (isPrime(c)):
+            return c
 
 def totient(n):
 
-  if n == 1:
-    return 1
-  if n < 1:
-    return None
-  
-  total = 0
-  for i in range (1, n):
-    if coPrime(i,n):
-      total += 1
+    if n == 1:
+        return 1
+    if n < 1:
+        return None
 
-  return total
+    total = 0
+    for i in range (1, n):
+        if coPrime(i,n):
+            total += 1
+
+    return total
 
 def primeTotient(p,q):
-  #totient for product of 2 primes
+    #totient for product of 2 primes
 
-  return (p-1) * (q-1)
+    return (p-1) * (q-1)
 
+def getE(p):
+    while True:
+        c = random.randint(3,p)
+        if coPrime(c,p) == 1:
+            return c
 
-print(RandomPrime(99999999,9999999999))
+def getD(e, p, seed = 3):
+    #encryption exponent, e
+    #totient phi(n), p
+
+    while True:
+        seed = random.randint(3,e)
+        d = (1 + seed * p) / e
+        print("d = {} = (1 + {} * {}) / {}".format(d,seed,p,e,))
+        #if integer
+        if d % 1 == 0:
+            return d
